870f8db972565c5f888fcc1b0cb0df58070b9d43
funtom4eg <a.paladyuk@gmail.com>
2017-01-18 13:55:22 +0200

    update 2.5

--- DemoWPF/MainWindow.xaml.cs
+++ DemoWPF/MainWindow.xaml.cs
@@ -23,6 +23,25 @@ namespace DemoWPF
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        class Person
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public Person[] Employees { get; set; }
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            lbMain.ItemsSource = new Person[]
+            { new Person() { FirstName = "Ivan", LastName = "Ivanov", Employees = new Person[]
+                { new Person() { FirstName = "name1", LastName = "sname1" }, new Person() {FirstName = "name2", LastName = "sname2"} } },
+              new Person() {FirstName = "Petr", LastName = "Petrov", Employees = new Person[]
+              { new Person() { FirstName = "name3", LastName = "sname3" }, new Person() {FirstName = "name4", LastName = "sname4"} } },
+              new Person {FirstName = "Sidor", LastName = "Sidorov", Employees = new Person[]
+              { new Person() { FirstName = "name5", LastName = "sname5" }, new Person() {FirstName = "name6", LastName = "sname6"} } } };
         }
     }
 }
